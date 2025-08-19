137e7323a1ed8c51957a5f44478e3b4482409612
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-19 11:26:00 +0000

    Creation de la page d'affichage des liste de tache avec son template urls et vues

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import DeleteView, ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import get_object_or_404
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from .models import Task
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'tasks/task_list.html'
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).order_by('-created_at')
