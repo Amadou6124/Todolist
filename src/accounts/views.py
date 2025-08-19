1c0642dba69752c6495348a1b5dbf8582875b9d7
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-19 12:12:21 +0000

    Ajout de formulaire dans accounts pour l'inscription et configurations general

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # ✅ Connexion automatique après inscription
+            return redirect('task-list')  # ou 'task_list' ou autre
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
