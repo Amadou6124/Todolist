21687daadda8e45a710e9481edbd469569457094
Amadou6124 <148112454+Amadou6124@users.noreply.github.com>
2025-08-19 11:40:14 +0000

    Mise en place de fonction pour modifier ou mettre a jour une tache

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskListView, TaskCreateView
+from .views import TaskListView, TaskCreateView, TaskUpdateView
 
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
     path('create/', TaskCreateView.as_view(), name='task_create'),
+    path('update/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -25,3 +25,12 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title', 'description']
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
